da605bef6446bc3c6a4535752472da8e66d343c9
Daniel Murrell <dsmurrell@gmail.com>
2016-11-07 12:51:00 +0000

    d

--- rest.py
+++ rest.py
@@ -1,9 +1,11 @@
 import requests
 
 def ob_api_get_profile(session_cookie, OB_HOST, OB_API_PREFIX, SESSION_COOKIE_NAME, guid):
-    r = requests.get(
-        u'{}{}profile?guid={}'.format(OB_HOST, OB_API_PREFIX, guid),
-        cookies={SESSION_COOKIE_NAME: session_cookie})
+    url = u'{}{}profile?guid={}'.format(OB_HOST, OB_API_PREFIX, guid)
+    print url
+    print SESSION_COOKIE_NAME
+    print session_cookie
+    r = requests.get(url, cookies={SESSION_COOKIE_NAME: session_cookie})
 
     assert r.status_code == 200
     return r.json()['profile']
